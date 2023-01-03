0e8c4dd79aa485ab83fc2838a161b103517d82cb
karinlee <karinlee92@gmail.com>
2023-01-03 13:12:41 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+{let score = 5 + 5 * 10;
+
+console.log('Ergebnis: ' + score);}
+
+{let score = (5 + 5) * 10;
+
+console.log('Ergebnis: ' + score);}
+
+{let score = 0;
+score = score + 10;
+
+console.log('Ergebnis:'+ score);}
+
+// shorthand
+let score = 0;
+score += 10;
+
+console.log('Ergebnis:'+ score);
+
+// Inkrementierung und Dekrementierung
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+zahl++;
+console.log('increment: ' + zahl);
+
+zahl--;
+console.log('decrement:'+ zahl);
